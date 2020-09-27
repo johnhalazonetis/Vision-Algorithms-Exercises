a91a701d97158483490df445bf55258d6d5dbdbe
John Halazonetis <john.halazonetis@icloud.com>
2020-09-27 13:01:26 +0200

    Made projectPoints function, need to draw points on image now and make function for drawing cube on image

--- src/ex1/ex1.cpp
+++ src/ex1/ex1.cpp
@@ -20,7 +20,6 @@ void readCalibrationMatrix(string& filename, Mat calibrationMatrix)     // Funct
     ifstream file;                                                      // Make an inward stream of data called 'file'
     file.open (datapath + filename);                                    // Open the file in question (we sum up the datapath and the filename)
     if (!file.is_open()) return;                                        // Insurance in case the file is not opened or doesn't exist
-
     double value;                                                       // Make variable 'value', where we put the values that we get from file                                  
     
     for (int r = 0; r < 3; r++)                                         // Make for loop to go through the rows of our calibration matrix
@@ -33,13 +32,12 @@ void readCalibrationMatrix(string& filename, Mat calibrationMatrix)     // Funct
     }
 }
 
-void makeTranslationMatrix(string& filename, Mat transformationMatrix)  // Function to get the poses from a text file and make a transformation matrix
+void makeTransforationMatrix(string& filename, Mat transformationMatrix)  // Function to get the poses from a text file and make a transformation matrix
 {
     ifstream file;                                                      // Make an inward stream of data called 'file'
     file.open (datapath + filename);                                    // Open the file in question (we sum up the datapath and the filename)
     if(!file.is_open()) return;                                         // Insurance in case the file is not opened or does not exist
-
-    double value;                                                       // Make a variable 'value', where we put the calues that we get from file
+    double value;                                                       // Make a variable 'value', where we put the values that we get from file
 
     // Get rotation values and make rotation matrix (Rodrigues formula)
     Mat omega(3, 1, CV_64F);                                            // Make a vector called 'omega', where we put out values of omega
@@ -77,9 +75,42 @@ void makeTranslationMatrix(string& filename, Mat transformationMatrix)  // Funct
     hconcat(rotationMatrix, translationMatrix, transformationMatrix);   // Concatenate the rotation matrix and the translation vector to the transformation matrix
 }
 
-void projectPoints(Mat& projectionMatrix, Mat pointCoordinates)
+void projectPoints(Mat& calibrationMatrix, Mat& transformationMatrix, Mat& inputWorldPoints, bool& lensDistortion, Mat outputCameraPoints)
 {
+    Mat worldPointsHomo = Mat::zeros(4, 1, CV_64F);                     // Define homogeneous vector for world points (empty)
+    vconcat(inputWorldPoints, Mat::eye(1, 1, CV_64F), worldPointsHomo); // Create homogeneous coordinates vector for world points
+
+    Mat cameraPointsHomo = Mat::zeros(4, 1, CV_64F);                    // Define the camera points vector in homogeneous coordinates
+    cameraPointsHomo = transformationMatrix * worldPointsHomo;          // Calculate the camera points in homogeneous coordinates
+
+    Mat normalizedCoordinates(2, 1 ,CV_64F);                            // Define the normalized coordinates vector
+    normalizedCoordinates.at<double>(0, 0) = cameraPointsHomo.at<double>(0, 0)/cameraPointsHomo.at<double>(2, 0);   // Calculate first element of the normalized coordate
+    normalizedCoordinates.at<double>(1, 0) = cameraPointsHomo.at<double>(1, 0)/cameraPointsHomo.at<double>(2, 0);   // Calculate second element of the normalized coordate
+
+    if (lensDistortion)                                                 // If we want to account for lens distortion as well
+    {
+        double r = sqrt(pow(normalizedCoordinates.at<double>(0, 0), 2) + pow(normalizedCoordinates.at<double>(1, 0), 2));   // Compute the radial component of normalized coordinates
+
+        ifstream file;                                                  // Make an inward stream of data called 'file'
+        file.open (datapath + "D.txt");                                 // Open the file in question (we sum up the datapath and the filename)
+        if(!file.is_open()) return;                                     // Insurance in case the file is not opened or does not exist
+        double k1, k2;                                                  // Make a variable 'k1' and 'k2', where we put the values that we get from file
+        file >> k1; file >> k2;                                         // Put values from distortion file into variables k1 and k2
+
+        normalizedCoordinates = (1 + k1*pow(r, 2) + k2*pow(r, 4)) * normalizedCoordinates;  // Compute the distorted normalized coordinates
+    }
     
+    Mat outputCameraPointsHomo(3, 1, CV_64F);                           // Define output camera points vector in homogeneous coordinates
+    Mat normalizedCoordinatesHomo(3, 1, CV_64F);                        // Define normalized camera coordinates vector in homogeneous coordinates
+    vconcat(normalizedCoordinates, Mat::eye(1, 1, CV_64F), normalizedCoordinatesHomo);  // Make normalized camera coordinates vector in homogeneous coordinates
+
+    outputCameraPointsHomo = calibrationMatrix * normalizedCoordinatesHomo; // Calculate output camera points in homogenous coordinates
+    
+    int cameraCoordinate0 = outputCameraPointsHomo.at<double>(0, 0) / outputCameraPointsHomo.at<double>(2, 0);  // Normalize output camera coordinates and convert to integer (for pixel coordinates)
+    int cameraCoordinate1 = outputCameraPointsHomo.at<double>(1, 0) / outputCameraPointsHomo.at<double>(2, 0);  // Normalize output camera coordinates and convert to integer (for pixel coordinates)
+
+    outputCameraPoints.at<double>(0, 0) = cameraCoordinate0;            // Input integer values to output camera coordinates vector
+    outputCameraPoints.at<double>(1, 0) = cameraCoordinate1;            // Input integer values to output camera coordinates vector
 }
 
 int main(int argc, char** argv)
@@ -98,10 +129,17 @@ int main(int argc, char** argv)
 
     Mat transformationMatrix(3, 4, CV_64F);                             // Define the transformation matrix for function 'makeTranslationMatrix'
     string posesFile = "poses.txt";                                     // Define the name of the poses text file
-    makeTranslationMatrix(posesFile, transformationMatrix);             // Call function to read the poses and create a transformation matrix
+    makeTransforationMatrix(posesFile, transformationMatrix);           // Call function to read the poses and create a transformation matrix
 
     Mat projectionMatrix(3, 4, CV_64F);                                 // Define projection matrix
     projectionMatrix = calibrationMatrix * transformationMatrix;        // Multiply calibration matrix by transformation matrix to make projection matrix
+
+    Mat inputWorldPoints = Mat::zeros(3,1, CV_64F);                     // Define input points vector (in world coordinates)
+    Mat outputCameraPoints(2, 1, CV_64F);                               // Define output points vector (in camera frame)
+    bool lensDistortion = 0;                                            // Define whether we want to have lens distortion on or off
+    projectPoints(calibrationMatrix, transformationMatrix, inputWorldPoints, lensDistortion, outputCameraPoints);  // Call function to project points from world frame to camera frame
+
+    cout << outputCameraPoints << endl;
 
     namedWindow("Display Image", WINDOW_KEEPRATIO);                     // Create window where the ratio of input image is kept
     imshow("Display Image", image);                                     // show the input image
