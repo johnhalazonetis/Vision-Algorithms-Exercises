1226db54d1666b74f941f538fe558d747a75ea99
John Halazonetis <john.halazonetis@icloud.com>
2020-09-27 18:49:18 +0200

    Restructured code to work with image series input, need to rewrite it to make working with many files at once easier (for project)

--- src/ex1/ex1.cpp
+++ src/ex1/ex1.cpp
@@ -30,6 +30,7 @@ void readCalibrationMatrix(string& filename, Mat calibrationMatrix)         // F
             calibrationMatrix.at<double>(r, c) = value;                 // Place each new value from the file into the calibration matrix
         }
     }
+    file.close();
 }
 
 void makeTransforationMatrix(string& filename, Mat transformationMatrix)    // Function to get the poses from a text file and make a transformation matrix
@@ -75,7 +76,7 @@ void makeTransforationMatrix(string& filename, Mat transformationMatrix)    // F
     hconcat(rotationMatrix, translationMatrix, transformationMatrix);   // Concatenate the rotation matrix and the translation vector to the transformation matrix
 }
 
-void projectPoints(Mat& calibrationMatrix, Mat& transformationMatrix, Mat& inputWorldPoints, bool& lensDistortion, Mat outputCameraPoints)
+void projectPoints(Mat& calibrationMatrix, Mat& transformationMatrix, Mat& inputWorldPoints, bool& lensDistortion, Mat outputCameraPoints)      // Function to project world coordinates onto image plane (with lens distortion correction if requested)
 {
     Mat worldPointsHomo = Mat::zeros(4, 1, CV_64F);                     // Define homogeneous vector for world points (empty)
     vconcat(inputWorldPoints, Mat::eye(1, 1, CV_64F), worldPointsHomo); // Create homogeneous coordinates vector for world points
@@ -96,15 +97,16 @@ void projectPoints(Mat& calibrationMatrix, Mat& transformationMatrix, Mat& input
         if(!file.is_open()) return;                                     // Insurance in case the file is not opened or does not exist
         double k1, k2;                                                  // Make a variable 'k1' and 'k2', where we put the values that we get from file
         file >> k1; file >> k2;                                         // Put values from distortion file into variables k1 and k2
+        file.close();
 
         normalizedCoordinates = (1 + k1*pow(r, 2) + k2*pow(r, 4)) * normalizedCoordinates;  // Compute the distorted normalized coordinates
     }
     
     Mat outputCameraPointsHomo(3, 1, CV_64F);                           // Define output camera points vector in homogeneous coordinates
     Mat normalizedCoordinatesHomo(3, 1, CV_64F);                        // Define normalized camera coordinates vector in homogeneous coordinates
-    vconcat(normalizedCoordinates, Mat::eye(1, 1, CV_64F), normalizedCoordinatesHomo);  // Make normalized camera coordinates vector in homogeneous coordinates
+    vconcat(normalizedCoordinates, Mat::eye(1, 1, CV_64F), normalizedCoordinatesHomo);      // Make normalized camera coordinates vector in homogeneous coordinates
 
-    outputCameraPointsHomo = calibrationMatrix * normalizedCoordinatesHomo; // Calculate output camera points in homogenous coordinates
+    outputCameraPointsHomo = calibrationMatrix * normalizedCoordinatesHomo;                 // Calculate output camera points in homogenous coordinates
     
     int cameraCoordinate0 = outputCameraPointsHomo.at<double>(0, 0) / outputCameraPointsHomo.at<double>(2, 0);  // Normalize output camera coordinates and convert to integer (for pixel coordinates)
     int cameraCoordinate1 = outputCameraPointsHomo.at<double>(1, 0) / outputCameraPointsHomo.at<double>(2, 0);  // Normalize output camera coordinates and convert to integer (for pixel coordinates)
@@ -151,8 +153,6 @@ void drawCube(Mat& image, Mat& cubeOrigin, double& length, Mat& calibrationMatri
     tempInputWorldCoords = cubeWorldCorners.col(7);
     projectPoints(calibrationMatrix, transformationMatrix, tempInputWorldCoords, lensDistortion, outputCameraCorners.col(7));
     
-    cout << outputCameraCorners << endl;
-    
     line(image, Point(outputCameraCorners.col(0)), Point(outputCameraCorners.col(1)), Scalar(0, 0, 255), 2, LINE_8);
     line(image, Point(outputCameraCorners.col(1)), Point(outputCameraCorners.col(2)), Scalar(0, 0, 255), 2, LINE_8);
     line(image, Point(outputCameraCorners.col(2)), Point(outputCameraCorners.col(3)), Scalar(0, 0, 255), 2, LINE_8);
@@ -172,13 +172,7 @@ void drawCube(Mat& image, Mat& cubeOrigin, double& length, Mat& calibrationMatri
 
 int main(int argc, char** argv)
 {
-    Mat image = imread(datapath + "images/img_0001.jpg");               // Define image in file
-
-    if (!image.data)                                                    // If statement in case the file cannot be opened or does not exist
-    {
-        printf("No image data \n");
-        return -1;                                                      // End program
-    }
+    // Initlialization of main:
 
     Mat calibrationMatrix(3, 3, CV_64F);                                // Define calibration matrix for function 'readCalibrationMatrixFile'
     string calibrationMatrixFile = "K.txt";                             // Define the name of the calibration matrix file
@@ -186,27 +180,42 @@ int main(int argc, char** argv)
 
     Mat transformationMatrix(3, 4, CV_64F);                             // Define the transformation matrix for function 'makeTranslationMatrix'
     string posesFile = "poses.txt";                                     // Define the name of the poses text file
-    makeTransforationMatrix(posesFile, transformationMatrix);           // Call function to read the poses and create a transformation matrix
-
-    Mat projectionMatrix(3, 4, CV_64F);                                 // Define projection matrix
-    projectionMatrix = calibrationMatrix * transformationMatrix;        // Multiply calibration matrix by transformation matrix to make projection matrix
+    
 
     Mat inputWorldPoints = Mat::zeros(3,1, CV_64F);                     // Define input points vector (in world coordinates)
     Mat outputCameraPoints(2, 1, CV_64F);                               // Define output points vector (in camera frame)
     bool lensDistortion = 1;                                            // Define whether we want to have lens distortion on or off
-    projectPoints(calibrationMatrix, transformationMatrix, inputWorldPoints, lensDistortion, outputCameraPoints);  // Call function to project points from world frame to camera frame
+    
 
-    circle(image, Point(outputCameraPoints.col(0)), 3, Scalar( 0, 0, 255 ), FILLED, LINE_8 );  // Draw point at inputWorldCoordinates
+    Mat cubeOrigin = 0.04*Mat::eye(3, 1, CV_64F);                       // Define where we want to place the cube on the chessboard
+    double cubeLength = 0.08;                                           // Define length of cube side
 
-    Mat cubeOrigin = 0.04*Mat::eye(3, 1, CV_64F);
-    double length = 0.08;
-    drawCube(image, cubeOrigin, length, calibrationMatrix, transformationMatrix, lensDistortion);
+    VideoCapture cap(datapath + "images/img_%04d.jpg");                 // Start video capture from images found in folder
+    int imageNumber = 1;                                                // Start counting images that we get as inputs
+    while( cap.isOpened() )                                             // Loop while we are receiving images from folder
+    {
+        Mat image;                                                          // Define image as matrix
+        cap.read(image);                                                    // Read image
 
-    imshow("Display Image", image);                                     // Show the input image
+        if (!image.data)                                                    // If statement in case the file cannot be opened or does not exist
+        {
+            printf("No image data \n");
+            return -1;                                                      // End program
+        }
 
-    moveWindow("Display Image", 500, 500);                              // Move the window in the screen
+        makeTransforationMatrix(posesFile, transformationMatrix);           // Call function to read the poses and create a transformation matrix
 
-    waitKey(0);
+        projectPoints(calibrationMatrix, transformationMatrix, inputWorldPoints, lensDistortion, outputCameraPoints);   // Call function to project points from world frame to camera frame
+
+        circle(image, Point(outputCameraPoints.col(0)), 3, Scalar( 0, 0, 255 ), FILLED, LINE_8 );                       // Draw point at inputWorldCoordinates
+
+        drawCube(image, cubeOrigin, cubeLength, calibrationMatrix, transformationMatrix, lensDistortion);               // Call function to draw cube at given cubeOrigin
+
+        imshow("Display Image", image);                                     // Show the input image
+
+        waitKey(15);                                                        // Wait for 15 ms
+        imageNumber++;                                                      // Increase imageNumber by 1
+    }
 
     return 0;
 }
