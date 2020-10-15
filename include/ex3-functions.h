9b35bed9d1c8c135575c1273d957c0b67f927237
John Halazonetis <john.halazonetis@icloud.com>
2020-10-15 16:14:28 +0200

    Started exercise 3

---
+++ include/ex3-functions.h
@@ -0,0 +1,98 @@
+MatrixXd describeKeypoints(Mat& img, int& keypoints, int r)
+{
+    /*
+    Returns a (2r+1)^2xN matrix of image patch vectors based on image
+    img and a 2xN matrix containing the keypoint coordinates.
+    r is the patch "radius".
+    */
+
+   MatrixXd descriptors;
+
+   // ...
+
+   return descriptors;
+}
+
+Mat harrisScores(Mat& img, int& patchSize, double& kappa)
+{
+    // Define kernels:
+    Matx33d Sobelx(-1, 0, 1, -2, 0, 2, -1, 0, 1);
+    Matx33d Sobely(-1, -2, -1, 0, 0, 0, 1, 2, 1);
+
+    // Convolve the images with the given sobel kernels:
+    Mat I_X; filter2D(img, I_X, -1, Sobelx);
+    Mat I_Y; filter2D(img, I_Y, -1, Sobely);
+
+    // Compute the M matrix with the given image patch size
+    Mat I_X2; multiply(I_X, I_X, I_X2);
+    Mat I_Y2; multiply(I_Y, I_Y, I_Y2);
+    Mat I_XI_Y; multiply(I_X, I_Y, I_XI_Y);
+
+    // Define a matrix of ones (for patch summing)
+    Mat ones = Mat::ones(patchSize, patchSize, CV_64F);
+
+    // Sum the values of the patch using the ones matrix
+    Mat I_X2Patch; filter2D(I_X2, I_X2Patch, -1, ones);
+    Mat I_Y2Patch; filter2D(I_Y2, I_Y2Patch, -1, ones);
+    Mat I_XI_YPatch; filter2D(I_XI_Y, I_XI_YPatch, -1, ones);
+    
+    // Add a border of (patchSize-1)/2 around each patch image
+
+    imshow("Test image", I_XI_YPatch);
+
+    Mat tempDet1; multiply(I_X2Patch, I_Y2Patch, tempDet1);
+    Mat tempDet2; multiply(I_XI_YPatch, I_XI_YPatch, tempDet2);
+    Mat tempTrace = I_X2Patch - I_Y2Patch;
+
+    // Compute the harris score for each pixel (from the M matrix)
+    Mat scores = tempDet1 - tempDet2 - kappa*pow(tempTrace, 2);
+
+    return scores;
+}
+
+VectorXd matchDescriptors(MatrixXd& query_descriptors, MatrixXd& databaseDescriptors, double lambda)
+{
+    /*
+    Returns a 1xQ matrix where the i-th coefficient is the index of the
+    database descriptor which matches to the i-th query descriptor.
+    The descriptor vectors are MxQ and MxD where M is the descriptor
+    dimension and Q and D the amount of query and database descriptors
+    respectively. matches(i) will be zero if there is no database descriptor
+    with an SSD < lambda * min(SSD). No two non-zero elements of matches will
+    be equal.
+    */
+
+   VectorXd matches;
+
+   // ...
+
+   return matches;
+}
+
+void plotMatches(VectorXd& matches, VectorXd queryKeypoints, VectorXd databaseKeypoints)
+{
+
+}
+
+MatrixXd selectKeypoints(MatrixXd& scores, int num, int r)
+{
+    /*
+    Selects the num best scores as keypoints and performs non-maximum 
+    supression of a (2r + 1)*(2r + 1) box around the current maximum.
+    */
+
+    MatrixXd keypoints;
+
+    // ...
+
+    return keypoints;
+}
+
+MatrixXd shiTomasi(Mat& img, int& patchSize)
+{
+    MatrixXd scores;
+
+    // ...
+
+    return scores;
+}
---
+++ src/ex3.cpp
@@ -0,0 +1,74 @@
+#include <Eigen/Core>               // Importing general Eigen library
+#include <Eigen/SVD>                // Allows to calculate SVD (using Jacobi or faster BDC methods)
+#include <Eigen/Dense>              // Allows to calulate the inverse of Eigen::Matrix
+#include <opencv2/core.hpp>
+#include <opencv2/core/eigen.hpp>
+#include <opencv2/imgcodecs.hpp>
+#include <opencv2/imgproc.hpp>
+#include <opencv2/highgui.hpp>
+
+#include <sstream>
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+using namespace cv;
+using namespace Eigen;
+
+// Include the functions that were made during exercises
+#include "base-functions.h"
+#include "ex3-functions.h"
+
+int main(int argc, char** argv)
+{
+    // Initialisation of main:
+    string datapath = "/home/john/Nextcloud/Me/ETH/Master 4 (Fall 2020)/Vision Algorithms/Exercises/Exercise 3 - Simple Keypoint Tracker/data/";    // Define path to data (for exercise 2)
+
+    // Randomly chosen parameters that seem to work well - can you find better ones?
+    int cornerPatchSize = 9;
+    double harrisKappa = 0.08;
+    int numKeypoints = 200;
+    int nonmaximumSupressionRadius = 8;
+    int descriptorRadius = 9;
+    int matchLambda = 4;
+
+    Mat scores, keypoints, desc, previousDesc, previousKeypoints;
+    VectorXd matches;
+    int frameNumber = 0;
+
+    VideoCapture cap(datapath + "%06d.png");                                                                                                        // Start video capture from images found in folder
+    while( cap.isOpened() )                                                                                                                         // Loop while we are receiving images from folder
+    {
+        Mat image;                                                                                                                                      // Define image as matrix
+        cap.read(image);                                                                                                                                // Read image
+
+        if (!image.data)                                                                                                                                // If statement in case the file cannot be opened or does not exist
+        {
+            printf("No image data \n");
+            return -1;                                                                                                                                  // End program
+        }
+
+        scores = harrisScores(image, cornerPatchSize, harrisKappa);
+        // keypoints = selectKeypoints(scores, numKeypoints, nonmaximumSupressionRadius);
+        // (plot the keypoints)
+
+        /* desc = describeKeypoints(image, cornerPatchSize, harrisKappa);
+
+        if (frameNumber != 0)
+        {
+            matches = matchDescriptors(desc, previousDesc, matchLambda);
+            plotMatches(matches, keypoints, previousKeypoints);
+        }
+
+        previousKeypoints = keypoints;
+        previousDesc = desc; */
+
+        // imshow("Display Image", image);                                                                                                                 // Show the input image
+
+        waitKey(1);                                                                                                                                    // Wait for X ms
+        frameNumber++;
+    }
+
+    destroyAllWindows();                                                                                                                            // Close all windows
+    return 0;
+}
