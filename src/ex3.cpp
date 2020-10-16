01f2216809c57ed9ca15463f48685a56ebd8164c
John Halazonetis <john.halazonetis@icloud.com>
2020-10-16 11:34:52 +0200

    Small modifications and corrections

--- src/ex3.cpp
+++ src/ex3.cpp
@@ -22,7 +22,7 @@ using namespace Eigen;
 int main(int argc, char** argv)
 {
     // Initialisation of main:
-    string datapath = "/home/john/Nextcloud/Me/ETH/Master 4 (Fall 2020)/Vision Algorithms/Exercises/Exercise 3 - Simple Keypoint Tracker/data/";    // Define path to data (for exercise 2)
+    string datapath = "/home/john/Nextcloud/Me/ETH/Master 4 (Fall 2020)/Vision Algorithms/Exercises/Exercise 3 - Simple Keypoint Tracker/data/";    // Define path to data (for exercise 3)
 
     // Randomly chosen parameters that seem to work well - can you find better ones?
     int cornerPatchSize = 9;
@@ -36,16 +36,16 @@ int main(int argc, char** argv)
     VectorXd matches;
     int frameNumber = 0;
 
-    VideoCapture cap(datapath + "%06d.png");                                                                                                        // Start video capture from images found in folder
-    while( cap.isOpened() )                                                                                                                         // Loop while we are receiving images from folder
+    VideoCapture cap(datapath + "%06d.png");                                                // Start video capture from images found in folder
+    while( cap.isOpened() )                                                                 // Loop while we are receiving images from folder
     {
-        Mat image;                                                                                                                                      // Define image as matrix
-        cap.read(image);                                                                                                                                // Read image
+        Mat image;                                                                          // Define image as matrix
+        cap.read(image);                                                                    // Read image
 
-        if (!image.data)                                                                                                                                // If statement in case the file cannot be opened or does not exist
+        if (!image.data)                                                                    // If statement in case the file cannot be opened or does not exist
         {
             printf("No image data \n");
-            return -1;                                                                                                                                  // End program
+            return -1;                                                                      // End program
         }
 
         scores = shiTomasi(image, cornerPatchSize);
@@ -63,12 +63,12 @@ int main(int argc, char** argv)
         previousKeypoints = keypoints;
         previousDesc = desc; */
 
-        imshow("Display Image", scores);                                                                                                                 // Show the input image
+        imshow("Display Image", scores);                                                    // Show the input image
 
-        waitKey(1);                                                                                                                                    // Wait for X ms
+        waitKey(1);                                                                         // Wait for X ms
         frameNumber++;
     }
 
-    destroyAllWindows();                                                                                                                            // Close all windows
+    destroyAllWindows();                                                                    // Close all windows
     return 0;
 }
